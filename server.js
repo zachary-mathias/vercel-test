002b4c0fb8d7edb1d98b9d636ef039f1a948b834
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-05-21 12:07:12 -0400

    deployment

--- server.js
+++ server.js
@@ -20,3 +20,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 const PORT = process.env.PORT
 
 app.listen(PORT, console.log(`listening on port ${PORT}`))
+
+module.exports = app
